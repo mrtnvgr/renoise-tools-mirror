4e9cfd329a87d4c11aae561ea2cf7f620123adc2
"mrtnvgr" <"martynovegorOF@yandex.ru">
2025-07-31 01:09:30 +0700

    v1.05

---
+++ Duplex.lua
@@ -0,0 +1,107 @@
+--[[===============================================================================================
+--- The Duplex Library
+===============================================================================================]]--
+
+--[[--
+
+]]
+
+-- include supporting classes
+
+rns = nil
+_trace_filters = nil
+--_trace_filters = {"^Matrix*"}
+--_trace_filters = {"^StateController*"}
+--_trace_filters = {"^Recorder*","^UISlider*"}
+_trace_filters = {".*"}
+
+_clibroot = "cLib/classes/"
+_xlibroot = "xLib/classes/"
+
+require (_clibroot.."cLib")
+require (_clibroot.."cDebug")
+require (_clibroot.."cObservable")
+require (_clibroot.."cColor")
+require (_clibroot.."cScheduler")
+require (_clibroot.."cProcessSlicer")
+
+require (_xlibroot.."xLib")
+require (_xlibroot.."xMessage")
+require (_xlibroot.."xMidiMessage")
+require (_xlibroot.."xAutomation")
+require (_xlibroot.."xBlockLoop")
+require (_xlibroot.."xTrack")
+require (_xlibroot.."xTransport")
+require (_xlibroot.."xSongPos")
+require (_xlibroot.."xPatternPos")
+require (_xlibroot.."xScale")
+require (_xlibroot.."xInstrument")
+require (_xlibroot.."xOscClient")
+require (_xlibroot.."xNoteColumn")
+require (_xlibroot.."xPatternSequencer")
+require (_xlibroot.."xSongSettings")
+require (_xlibroot.."xCursorPos")
+
+
+-- include the duplex core, applications and device configurations
+require "Duplex/Globals"
+require "Duplex/Preferences"
+require "Duplex/Message"
+require "Duplex/MessageStream"
+require "Duplex/Display"
+require "Duplex/StateController"
+require "Duplex/WidgetHooks"
+require "Duplex/WidgetKeyboard"
+require "Duplex/Canvas"
+require "Duplex/CanvasPoint"
+require "Duplex/UIComponent"
+require "Duplex/UIButtonStrip"
+require "Duplex/UIButton"
+require "Duplex/UISlider"
+require "Duplex/UISpinner"
+require "Duplex/UIPad"
+require "Duplex/UIKey"
+require "Duplex/UILabel"
+require "Duplex/UILed"
+require "Duplex/ControlMap"
+require "Duplex/Device"
+require "Duplex/MidiDevice"
+require "Duplex/OscDevice"
+require "Duplex/OscVoiceMgr"
+require "Duplex/Application"
+require "Duplex/Automateable" -- extends Application
+require "Duplex/RoamingDSP" -- extends Automateable
+require "Duplex/Options"
+require "Duplex/Browser"
+require "Duplex/BrowserProcess"
+
+
+-- load all application scripts dynamically (Applications/XXX.lua)
+
+for _, filename in pairs(os.filenames("./Duplex/Applications", "*.lua")) do
+  local app_name = split_filename(filename)
+  require("Duplex/Applications/" .. app_name)
+end
+
+
+-- load all controller scripts dynamically (Controllers/XXX/XXX.lua)
+
+for _, foldername in pairs(os.dirnames("./Duplex/Controllers")) do
+  local subpath = "./Duplex/Controllers/" .. foldername
+
+  for _, filename in pairs(os.filenames(subpath, "*.lua")) do
+    -- only load the controller file that matches the controller folder name
+    if (split_filename(filename) == foldername) then
+      require(subpath .. "/" .. split_filename(filename))
+    end
+  end
+
+  -- include any device configurations (Controller/Configurations/XXX.lua)
+  -- TODO error message when configuration folder is missing
+  subpath = "./Duplex/Controllers/" .. foldername .. "/Configurations"
+  for _, filename in pairs(os.filenames(subpath, "*.lua")) do
+    require(subpath .. "/" .. split_filename(filename))
+  end
+
+end
+
---
+++ Duplex/Controllers/ZoomR16/ZoomR16.lua
@@ -0,0 +1,24 @@
+--[[----------------------------------------------------------------------------
+-- Duplex.ZoomR16
+----------------------------------------------------------------------------]]--
+
+--[[
+
+Inheritance: ZoomR16 > MidiDevice > Device
+
+A device-specific class 
+
+--]]
+
+
+--==============================================================================
+
+class "ZoomR16" (MidiDevice)
+
+function ZoomR16:__init(display_name, message_stream, port_in, port_out)
+
+  MidiDevice.__init(self, display_name, message_stream, port_in, port_out)
+
+end
+
+
